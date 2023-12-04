50b08c6dd1410a6c4133730a83737c904f3c0fa7
周小平 <zhouxiaoping@piesat.cn>
2023-12-04 13:05:59 +0800

    1、增加数据源mapper 2、增加表字段mapper

---
+++ spaceXP-tools/spaceXP-generator/src/main/java/cn/piesat/tools/generator/mapper/DataSourceMapper.java
@@ -0,0 +1,17 @@
+package cn.piesat.tools.generator.mapper;
+
+import cn.piesat.tools.generator.model.entity.DataSourceEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p/>
+ * {@code @description}  :数据源Mapper
+ * <p/>
+ * <b>@create:</b> 2023/12/4 11:29.
+ *
+ * @author zhouxp
+ */
+@Mapper
+public interface DataSourceMapper extends BaseMapper<DataSourceEntity> {
+}
---
+++ spaceXP-tools/spaceXP-generator/src/main/java/cn/piesat/tools/generator/mapper/TableFieldMapper.java
@@ -0,0 +1,18 @@
+package cn.piesat.tools.generator.mapper;
+
+import cn.piesat.tools.generator.model.entity.TableFieldEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p/>
+ * {@code @description}  :表字段mapper
+ * <p/>
+ * <b>@create:</b> 2023/12/4 10:55.
+ *
+ * @author zhouxp
+ */
+@Mapper
+public interface TableFieldMapper extends BaseMapper<TableFieldEntity> {
+
+}
