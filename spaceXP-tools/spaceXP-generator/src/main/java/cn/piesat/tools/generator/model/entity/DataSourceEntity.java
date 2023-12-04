f4502d3a8f6ed44efd023f6316e2ecad5861d0c9
周小平 <zhouxiaoping@piesat.cn>
2023-12-04 10:20:45 +0800

    1、创建数据源实体类 2、创建项目实体类

---
+++ spaceXP-tools/spaceXP-generator/src/main/java/cn/piesat/tools/generator/model/entity/DataSourceEntity.java
@@ -0,0 +1,62 @@
+package cn.piesat.tools.generator.model.entity;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * <p/>
+ * {@code @description}  :数据源实体类
+ * <p/>
+ * <b>@create:</b> 2023/12/4 9:32.
+ *
+ * @author zhouxp
+ */
+@Data
+@TableName("gen_datasource")
+public class DataSourceEntity {
+    /**
+     * id
+     */
+    @TableId
+    private Long id;
+    /**
+     * 数据库类型
+     */
+    private String dbType;
+    /**
+     * ip地址
+     */
+    private String ipAddr;
+    /**
+     * 端口
+     */
+    private Integer port;
+    /**
+     * 数据库名称
+     */
+    private String databaseName;
+    /**
+     * 连接名
+     */
+    private String connName;
+    /**
+     * URL
+     */
+    private String connUrl;
+    /**
+     * 用户名
+     */
+    private String username;
+    /**
+     * 密码
+     */
+    private String password;
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+}
---
+++ spaceXP-tools/spaceXP-generator/src/main/java/cn/piesat/tools/generator/model/entity/ProjectEntity.java
@@ -0,0 +1,85 @@
+package cn.piesat.tools.generator.model.entity;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * <p/>
+ * {@code @description}  :项目实体类
+ * <p/>
+ * <b>@create:</b> 2023/12/4 9:55.
+ *
+ * @author zhouxp
+ */
+@Data
+@TableName("gen_project")
+public class ProjectEntity {
+    /**
+     * id
+     */
+    @TableId
+    private Long id;
+
+    /**
+     * 项目名
+     */
+    private String name;
+
+    /**
+     * 是否是项目 1是 0 否
+     */
+    private Integer isProject;
+
+    /**
+     * 项目类型
+     */
+    private String projectType;
+
+    /**
+     * 组织机构ID
+     */
+    private String groupId;
+
+    /**
+     * 项目id
+     */
+    private String artifactId;
+
+    /**
+     * 版本
+     */
+    private String version;
+
+    /**
+     * 描述
+     */
+    private String description;
+
+    /**
+     * 作者
+     */
+    private String author;
+
+    /**
+     * 作者email
+     */
+    private String  email;
+
+    /**
+     * 端口
+     */
+    private Integer port;
+
+    /**
+     * 是否当前项目 1为当前 0为否
+     */
+    private Integer isCurrent;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+}
