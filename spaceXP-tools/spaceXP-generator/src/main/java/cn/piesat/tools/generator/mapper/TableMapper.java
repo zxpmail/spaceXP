c2833594414271b17954da066547b6aa9b2fa7a0
周小平 <zhouxiaoping@piesat.cn>
2023-12-04 10:48:00 +0800

    增加表mapper类

---
+++ spaceXP-tools/spaceXP-generator/src/main/java/cn/piesat/tools/generator/mapper/TableMapper.java
@@ -0,0 +1,17 @@
+package cn.piesat.tools.generator.mapper;
+
+import cn.piesat.tools.generator.model.entity.TableEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p/>
+ * {@code @description}  :表Mapper
+ * <p/>
+ * <b>@create:</b> 2023/12/4 10:46.
+ *
+ * @author zhouxp
+ */
+@Mapper
+public interface TableMapper extends BaseMapper<TableEntity> {
+}
