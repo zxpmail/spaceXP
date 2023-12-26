66354e79df5e995a325dbca284da97a7a56d8e27
周小平 <zhouxiaoping@piesat.cn>
2023-12-26 17:56:42 +0800

    1、增加代码生成信息类 2、增加模板信息类

---
+++ spaceXP-tools/spaceXP-generator/src/main/java/cn/piesat/tools/generator/model/GeneratorInfo.java
@@ -0,0 +1,20 @@
+package cn.piesat.tools.generator.model;
+
+import cn.piesat.tools.generator.model.vo.ProjectVO;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * <p/>
+ * {@code @description}  : 代码生成信息
+ * <p/>
+ * <b>@create:</b> 2023/12/26 17:54.
+ *
+ * @author zhouxp
+ */
+@Data
+public class GeneratorInfo {
+    private ProjectVO  projectVO;
+    private List<TemplateInfo> templates;
+}
