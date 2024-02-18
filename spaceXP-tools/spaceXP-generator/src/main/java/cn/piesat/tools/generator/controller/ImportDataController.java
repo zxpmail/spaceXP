e1763ea5430cdf183e4d017347f1741b022c949e
周小平 <zhouxiaoping@piesat.cn>
2024-02-18 19:04:11 +0800

    1、修改oracle版本 2、修改导入功能

--- spaceXP-tools/spaceXP-generator/src/main/java/cn/piesat/tools/generator/controller/ImportDataController.java
+++ spaceXP-tools/spaceXP-generator/src/main/java/cn/piesat/tools/generator/controller/ImportDataController.java
@@ -1,14 +1,10 @@
 package cn.piesat.tools.generator.controller;
 
 import cn.piesat.tools.generator.model.dto.ImportDataSourceDTO;
-import cn.piesat.tools.generator.model.entity.TableDO;
 import cn.piesat.tools.generator.model.vo.TableVO;
 import cn.piesat.tools.generator.service.ImportDataService;
-import javafx.scene.control.Tab;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
