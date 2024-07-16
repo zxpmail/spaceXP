f1330cf471b2f66456f1cbef70c49b42333f92c8
周小平 <zhouxiaoping@piesat.cn>
2024-07-16 17:01:52 +0800

    1、单元测试模板类

---
+++ spaceXP-tools/spaceXP-generator/src/test/java/cn/piesat/tools/generator/config/TemplatesConfigTest.java
@@ -0,0 +1,32 @@
+package cn.piesat.tools.generator.config;
+
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import javax.annotation.Resource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * <p/>
+ * {@code @description}: 模板配置测试类
+ * <p/>
+ * {@code @create}: 2024-07-16 14:55
+ * {@code @author}: zhouxp
+ */
+@SpringBootTest
+public class TemplatesConfigTest {
+    @Resource
+    private TemplatesConfig templatesConfig;
+
+    @Test
+    public void TestTemplatesConfig(){
+        assertEquals(templatesConfig.getTemplates().size(),2);
+        assertNotNull(templatesConfig.getDeveloper());
+        assertNotNull(templatesConfig.getProject());
+    }
+}
