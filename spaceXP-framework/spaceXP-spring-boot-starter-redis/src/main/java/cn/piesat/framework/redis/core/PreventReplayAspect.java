95ee84e9c3a02bd93ad7836812aed742e66526b5
周小平 <zhouxiaoping@piesat.cn>
2024-07-24 16:29:37 +0800

    1、修改防刷功能

--- spaceXP-framework/spaceXP-spring-boot-starter-redis/src/main/java/cn/piesat/framework/redis/core/PreventReplayAspect.java
+++ spaceXP-framework/spaceXP-spring-boot-starter-redis/src/main/java/cn/piesat/framework/redis/core/PreventReplayAspect.java
@@ -2,18 +2,22 @@ package cn.piesat.framework.redis.core;
 
 import cn.piesat.framework.common.constants.CommonConstants;
 import cn.piesat.framework.common.exception.BaseException;
+import cn.piesat.framework.common.utils.ArgsUtils;
 import cn.piesat.framework.redis.annotation.PreventReplay;
+import cn.piesat.framework.redis.utils.Md5Util;
 import com.alibaba.fastjson2.JSON;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 import java.lang.reflect.Method;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -48,32 +52,29 @@ public final class PreventReplayAspect {
     @Before("pointcut()")
     public void joinPoint(JoinPoint joinPoint) throws Exception {
         Object[] args = joinPoint.getArgs();
-        if (args == null || args.length == 0) {
-            throw new BaseException("[防刷]入参不允许为空");
-        }
-        String requestStr = JSON.toJSONString(args[0]);
-        if (!StringUtils.hasText(requestStr) || requestStr.equalsIgnoreCase("{}")) {
-            throw new BaseException("[防刷]入参不允许为空");
-        }
+        List<Object> list = ArgsUtils.processArgs(args);
 
         MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
         Method method = joinPoint.getTarget().getClass().getMethod(methodSignature.getName(),
                 methodSignature.getParameterTypes());
 
         PreventReplay preventAnnotation = method.getAnnotation(PreventReplay.class);
-        String methodFullName = method.getDeclaringClass().getName() + method.getName();
-        defaultHandle(requestStr, preventAnnotation, methodFullName);
+        String md5 = method.getDeclaringClass().getName() + method.getName();
+
+        if (!CollectionUtils.isEmpty(list)) {
+            md5 = JSON.toJSONString(list) + md5;
+        }
+        defaultHandle(md5, preventAnnotation);
     }
 
     /**
      * 默认处理方式
      */
-    private void defaultHandle(String requestStr, PreventReplay preventReplay, String methodFullName)  {
-        String base64Str = toBase64String(requestStr);
+    private void defaultHandle(String md5, PreventReplay preventReplay) {
         long expire = preventReplay.value();
-        String resp = redisService.getObject(keyPrefix + CommonConstants.UNDERLINE + methodFullName + base64Str);
+        String resp = redisService.getObject(keyPrefix + CommonConstants.UNDERLINE + md5);
         if (!StringUtils.hasText(resp)) {
-            redisService.setObject(keyPrefix + CommonConstants.UNDERLINE + methodFullName + base64Str, requestStr, expire, TimeUnit.SECONDS);
+            redisService.setObject(keyPrefix + CommonConstants.UNDERLINE + md5, md5, expire, TimeUnit.SECONDS);
         } else {
             String message = StringUtils.hasText(preventReplay.message()) ? preventReplay.message() :
                     expire + "秒内不允许重复请求";
