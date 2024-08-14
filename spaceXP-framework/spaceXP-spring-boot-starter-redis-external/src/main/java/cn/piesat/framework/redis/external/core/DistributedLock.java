2ba71d42d34a57942a5fe84bbc96d24f5f61611b
周小平 <zhouxiaoping@piesat.cn>
2024-08-14 18:02:20 +0800

    1、增加分布式锁接口

---
+++ spaceXP-framework/spaceXP-spring-boot-starter-redis-external/src/main/java/cn/piesat/framework/redis/external/core/DistributedLock.java
@@ -0,0 +1,28 @@
+package cn.piesat.framework.redis.external.core;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * <p/>
+ * {@code @description}: 分布式接口
+ * <p/>
+ * {@code @create}: 2024-08-14 18:01
+ * {@code @author}: zhouxp
+ */
+public interface DistributedLock {
+    boolean tryLock(long waitTime, long leaseTime, TimeUnit unit) throws InterruptedException;
+
+    boolean tryLock(long waitTime,  TimeUnit unit) throws InterruptedException;
+
+    boolean tryLock() throws InterruptedException;
+
+    void lock(long leaseTime, TimeUnit unit);
+
+    void unlock();
+
+    boolean isLocked();
+
+    boolean isHeldByThread(long threadId);
+
+    boolean isHeldByCurrentThread();
+}
