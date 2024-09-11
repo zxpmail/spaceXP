18f7342cb4ebc363612edefcf9a8dc45ea8a2602
周小平 <zhouxiaoping@piesat.cn>
2024-09-11 14:41:35 +0800

    1、修改包引入问题

--- spaceXP-tests/spaceXP-spring-boot-tests-web/src/main/java/cn/piesat/test/file/WebApplication.java
+++ spaceXP-tests/spaceXP-spring-boot-tests-web/src/main/java/cn/piesat/test/file/WebApplication.java
@@ -1,11 +1,9 @@
 package cn.piesat.test.file;
 
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
