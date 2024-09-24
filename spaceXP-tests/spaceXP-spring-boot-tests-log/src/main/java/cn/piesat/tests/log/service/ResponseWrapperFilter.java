b9f49de68aa19223ace075e05b93a795c4a8b770
周小平 <zhouxiaoping@piesat.cn>
2024-09-24 13:53:38 +0800

    1、由于aop记录日志多线程比较麻烦，对日记记录可能修改成OncePerRequestFilter进行

--- spaceXP-tests/spaceXP-spring-boot-tests-log/src/main/java/cn/piesat/tests/log/service/ResponseWrapperFilter.java
+++ spaceXP-tests/spaceXP-spring-boot-tests-log/src/main/java/cn/piesat/tests/log/service/ResponseWrapperFilter.java
@@ -1,15 +1,22 @@
 package cn.piesat.tests.log.service;
+
+import cn.piesat.framework.log.annotation.OpLog;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.HandlerExecutionChain;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.util.List;
 
 /**
  * <p/>
@@ -20,19 +27,28 @@ import java.io.IOException;
  */
 
 
-
 @Slf4j
-@Component
+//@Component
 public class ResponseWrapperFilter extends OncePerRequestFilter {
+    @SneakyThrows
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
+        Object handler = request.getAttribute(HandlerMapping.BEST_MATCHING_HANDLER_ATTRIBUTE);
+        if (handler instanceof HandlerMethod) {
+            OpLog oplog = ((HandlerMethod) handler).getMethod().getAnnotation(OpLog.class);
+            if(oplog == null){
+                filterChain.doFilter(request, response);
+                return;
+            }
+        }
         ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
-        ContentCachingRequestWrapper  requestWrapper = new ContentCachingRequestWrapper(request);
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
         requestWrapper.getParameterNames();
+
         String requestBody = new String(requestWrapper.getContentAsByteArray(), requestWrapper.getCharacterEncoding());
         log.info("@ request User-Agent: " + request.getHeader("User-Agent"));
         log.info("@ request body: " + requestBody);
+
 
         filterChain.doFilter(requestWrapper, responseWrapper);
         String responseBody = new String(responseWrapper.getContentAsByteArray(), responseWrapper.getCharacterEncoding());
@@ -47,4 +63,5 @@ public class ResponseWrapperFilter extends OncePerRequestFilter {
         // 必须调用此方法以将响应数据发送到客户端
         responseWrapper.copyBodyToResponse();
     }
+
 }
