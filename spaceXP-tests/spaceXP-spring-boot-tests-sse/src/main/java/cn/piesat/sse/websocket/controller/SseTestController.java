194c7096b26e8ceee292bd524c63a6cd853c8dd4
周小平 <zhouxiaoping@piesat.cn>
2024-09-26 10:48:38 +0800

    1、创建sse测试项目

---
+++ spaceXP-tests/spaceXP-spring-boot-tests-sse/src/main/java/cn/piesat/sse/websocket/controller/SseTestController.java
@@ -0,0 +1,54 @@
+package cn.piesat.sse.websocket.controller;
+
+
+import cn.piesat.framework.common.annotation.NoApiResult;
+import cn.piesat.framework.common.model.entity.MessageEntity;
+import cn.piesat.framework.redis.core.RedisService;
+import cn.piesat.framework.sse.core.SseClient;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+
+
+/**
+ * <p/>
+ * {@code @description}  :
+ * <p/>
+ * <b>@create:</b> 2024-06-11 9:16.
+ *
+ * @author zhouxp
+ */
+@RestController
+@RequestMapping("/sse")
+@CrossOrigin
+public class SseTestController {
+
+    @Resource
+    private SseClient sseClient;
+    @Resource
+    private RedisService redisService;
+
+    @GetMapping("/sendMessage")
+    public void sendMessage() {
+        MessageEntity messageEntity = new MessageEntity();
+        messageEntity.setAppId(1);
+        messageEntity.setToId(1001L);
+        messageEntity.setFromId(1L);
+        messageEntity.setType(1);
+        messageEntity.setTitle("hello");
+        messageEntity.setBody("hello world " + LocalDateTime.now());
+        redisService.convertAndSend("TOPIC", messageEntity);
+    }
+
+    @NoApiResult
+    @GetMapping(value = "/login")
+    public SseEmitter login(String userId, String appId) {
+        return sseClient.createSession(userId,appId);
+    }
+}
---
+++ spaceXP-tests/spaceXP-spring-boot-tests-sse/src/main/java/cn/piesat/sse/websocket/service/impl/RedisMessageServiceImpl.java
@@ -0,0 +1,41 @@
+package cn.piesat.sse.websocket.service.impl;
+
+import cn.piesat.framework.common.model.entity.MessageEntity;
+import cn.piesat.framework.redis.core.RedisMessageService;
+import cn.piesat.framework.sse.core.SseClient;
+import cn.piesat.framework.sse.util.SseSessionHolder;
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import javax.annotation.Resource;
+import java.util.Objects;
+
+
+/**
+ * <p/>
+ * {@code @description}: redis消息处理实现类
+ * <p/>
+ * {@code @create}: 2024-09-25 16:59
+ * {@code @author}: zhouxp
+ */
+@Service
+@Slf4j
+public class RedisMessageServiceImpl implements RedisMessageService {
+    @Resource
+    private SseClient sseClient;
+    @Override
+    public void handle(MessageEntity messageEntity) {
+        if (ObjectUtils.isEmpty(messageEntity)){
+            return;
+        }
+        Integer appId = messageEntity.getAppId();
+        Long toId = messageEntity.getToId();
+        SseEmitter session = SseSessionHolder.getSession(String.valueOf(toId), String.valueOf(appId));
+        String s = JSON.toJSONString(messageEntity);
+        sseClient.sendMessage(String.valueOf(toId), String.valueOf(appId),String.valueOf(s.hashCode()),s);
+        log.info("收到消息: {}, 类型: {} 消息标题:{}",messageEntity.getBody(),messageEntity.getType(),messageEntity.getTitle());
+    }
+}
