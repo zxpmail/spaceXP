ef0550d214e22995ff9266019a3b14c5cd9504a8
周小平 <zhouxiaoping@piesat.cn>
2024-10-12 11:22:45 +0800

    1、字节顺序枚举类

---
+++ spaceXP-framework/spaceXP-spring-boot-starter-netty/src/main/java/cn/piesat/framework/netty/model/enums/ByteOrderEnum.java
@@ -0,0 +1,19 @@
+package cn.piesat.framework.netty.model.enums;
+
+/**
+ * <p/>
+ * {@code @description}: 字节顺序枚举类
+ * <p/>
+ * {@code @create}: 2024-10-12 11:17
+ * {@code @author}: zhouxp
+ */
+public enum ByteOrderEnum {
+    /**
+     * 大端字节顺序
+     */
+    BIG_ENDIAN,
+    /**
+     * 小端字节顺序
+     */
+    LITTLE_ENDIAN,
+}
