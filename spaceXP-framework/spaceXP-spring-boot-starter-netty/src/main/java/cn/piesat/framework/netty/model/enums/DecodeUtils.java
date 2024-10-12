5a027fd8be171c5d9f1d60dbe7d8fcfede72099c
周小平 <zhouxiaoping@piesat.cn>
2024-10-12 14:35:58 +0800

    1、修改类型枚举 2、修改解码工具类

--- spaceXP-framework/spaceXP-spring-boot-starter-netty/src/main/java/cn/piesat/framework/netty/model/enums/DecodeUtils.java
+++ spaceXP-framework/spaceXP-spring-boot-starter-netty/src/main/java/cn/piesat/framework/netty/model/enums/DecodeUtils.java
@@ -1,5 +1,8 @@
 package cn.piesat.framework.netty.model.enums;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+
 /**
  * <p/>
  * {@code @description}: 解码工具类
@@ -8,4 +11,204 @@ package cn.piesat.framework.netty.model.enums;
  * {@code @author}: zhouxp
  */
 public final class DecodeUtils {
+
+    private DecodeUtils() {
+
+    }
+
+    /**
+     * 解码
+     *
+     * @param symbol  符号
+     * @param byteNum 字节数
+     * @param buff    数据
+     * @param type    枚举类型字符串
+     * @param endian  编码
+     * @return 解码数据
+     */
+    public static Object decode(String symbol, int byteNum, ByteBuf buff,
+                                String type, boolean endian) {
+        Object value;
+        final TypeEnum typeEnum = TypeEnum.match(type);
+        if (typeEnum == null) {
+            throw new IllegalArgumentException("Invalid type: " + type);
+        }
+
+        if (buff.readableBytes() < byteNum) {
+            throw new IndexOutOfBoundsException("Insufficient bytes in buffer");
+        }
+        switch (typeEnum) {
+            case HEX_STRING:
+                value = readHexString(byteNum, buff);
+                break;
+            case USHORT:
+                value = readUnSignShort(buff, endian);
+                break;
+            case SHORT:
+                value = readShort(buff, endian);
+                break;
+            case INT:
+                value = readInt(buff, endian);
+                break;
+            case UINT:
+                value = readUnSignInt(buff, endian);
+                break;
+            case BYTE:
+                value = readByte(buff);
+                break;
+            case UBYTE:
+                value = readUnSignByte(buff);
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported type: " + type);
+        }
+
+        return value;
+    }
+
+    /**
+     * 读无符号byte
+     *
+     * @param buff 编码数据
+     * @return 解码数据
+     */
+    public static short readUnSignByte(ByteBuf buff) {
+        if (!buff.isReadable(1)) {
+            throw new IllegalArgumentException("Buffer does not have enough readable bytes.");
+        }
+        byte by = buff.readByte();
+        return (short) (by & 0x0FF);
+    }
+
+    /**
+     * 读byte
+     *
+     * @param buff 编码数据
+     * @return 解码数据
+     */
+    public static byte readByte(ByteBuf buff) {
+        // 检查是否有足够的字节可以读取
+        if (!buff.isReadable(1)) {
+            throw new IllegalArgumentException("Buffer does not have enough readable bytes.");
+        }
+        return buff.readByte();
+    }
+
+    /**
+     * 读无符号int
+     *
+     * @param buff   编码数据
+     * @param endian 字节序
+     * @return 解码数据
+     */
+    public static long readUnSignInt(ByteBuf buff, boolean endian) {
+        if (buff == null || buff.readableBytes() < Integer.BYTES) {
+            throw new IllegalArgumentException("ByteBuf is null or too short to read an integer.");
+        }
+        int intValue = endian ? buff.readIntLE() : buff.readInt();
+        return intValue & 0x0FFFFFFFFL;
+    }
+
+    /**
+     * 读int
+     *
+     * @param buff   编码数据
+     * @param endian 字节序
+     * @return 解码数据
+     */
+    public static int readInt(ByteBuf buff, boolean endian) {
+        if (buff.readableBytes() < Integer.BYTES) {
+            throw new IndexOutOfBoundsException("Not enough bytes to read a int value.");
+        }
+        return endian ? buff.readIntLE() : buff.readInt();
+    }
+
+    /**
+     * 读short
+     *
+     * @param buff   编码数据
+     * @param endian 字节序
+     * @return 解码数据
+     */
+    public static short readShort(ByteBuf buff, boolean endian) {
+        if (buff.readableBytes() < Short.BYTES) {
+            throw new IndexOutOfBoundsException("Not enough bytes to read a short value.");
+        }
+        return endian ? buff.readShortLE() : buff.readShort();
+    }
+
+    /**
+     * 读无符号short
+     *
+     * @param buff   编码数据
+     * @param endian 字节序
+     * @return 解码数据
+     */
+    public static int readUnSignShort(ByteBuf buff, boolean endian) {
+        if (buff.readableBytes() < Short.BYTES) {
+            throw new IndexOutOfBoundsException("Not enough bytes to read a short value.");
+        }
+        short shortValue = endian ? buff.readShortLE() : buff.readShort();
+        return shortValue & 0x0FFFF;
+    }
+
+    /**
+     * 读Hex字符串
+     *
+     * @param num  字节长度
+     * @param buff 编码数据
+     * @return 字符串
+     */
+    public static String readHexString(int num, ByteBuf buff) {
+        String value = ByteBufUtil.hexDump(buff, 0, num);
+        readByteBuf(num, buff);
+        return value;
+    }
+
+    /**
+     * 读Hex字符串没有数据缓冲区偏移
+     *
+     * @param num  字节长度
+     * @param buff 编码数据
+     * @return 字符串
+     */
+    public static String readHexStringWithoutOffset(int num, ByteBuf buff) {
+        if (buff == null) {
+            throw new IllegalArgumentException("ByteBuf cannot be null");
+        }
+        int readableBytes = buff.readableBytes();
+        if (num < 0 || num > readableBytes) {
+            throw new IllegalArgumentException("Invalid num: " + num + ". Should be between 0 and " + readableBytes);
+        }
+        return ByteBufUtil.hexDump(buff, 0, num);
+    }
+
+    /**
+     * 移动读指针
+     *
+     * @param num  移动字节数
+     * @param buff 数据缓冲区ByteBuf
+     */
+    private static void readByteBuf(int num, ByteBuf buff) {
+        // 防止空指针异常
+        if (buff == null) {
+            throw new IllegalArgumentException("ByteBuf cannot be null");
+        }
+
+        // 确保num的合法性
+        if (num < 1) {
+            throw new IllegalArgumentException("Number of bytes to read must be at least 1");
+        }
+
+        // 避免超出Buffer长度的读取
+        if (num > buff.readableBytes()) {
+            throw new IndexOutOfBoundsException("Not enough readable bytes in buffer");
+        }
+        // 统一读取逻辑
+        if (num == 1) {
+            buff.readByte();
+        } else {
+            buff.readBytes(num);
+        }
+    }
 }
--- spaceXP-framework/spaceXP-spring-boot-starter-netty/src/main/java/cn/piesat/framework/netty/model/enums/TypeEnum.java
+++ spaceXP-framework/spaceXP-spring-boot-starter-netty/src/main/java/cn/piesat/framework/netty/model/enums/TypeEnum.java
@@ -9,87 +9,46 @@ package cn.piesat.framework.netty.model.enums;
  */
 public enum TypeEnum {
     /**
-     * 字符串
-     */
-    TYPE_STRING("string"),
-
-    /**
-     * Binary-Coded Decimal
-     * bcd码 8421码
-     * 4位二进制数表示1位十进制数
-     */
-    TYPE_BCD8421("bcd8421"),
-    /**
      * 时间字符串
      */
     TYPE_DATE_STRING("date_string"),
-    /**
-     * 枚举byte
-     */
-    TYPE_ENUM_BYTE("enum|byte"),
-
-    /**
-     * 枚举int
-     */
-    TYPE_ENUM_INT("enum|int"),
-
-    /**
-     * 枚举字符串
-     */
-    TYPE_ENUM_STRING("enum|string"),
-
-    /**
-     * 枚举HEX字符串
-     */
-    TYPE_ENUM_HEX_STRING("enum|hex_string"),
 
     /**
      * HEX字符串
      */
-    TYPE_HEX_STRING("hex_string"),
-
+    HEX_STRING("hex_string"),
     /**
      * -2^31~2^31-1
      * -2,147,483,648~2,147,483,647
      */
-    TYPE_INT("int"),
+    INT("int"),
     /**
      * 0~2^32
      * 0~4294967296L
      */
-    TYPE_UINT("uint"),
+    UINT("uint"),
     /**
      * -2^15~2^15-1
      * -32768~32767
      */
-    TYPE_SHORT("short"),
+    SHORT("short"),
     /**
      * 0~65535
      */
-    TYPE_USHORT("ushort"),
+    USHORT("ushort"),
     /**
      * -2^7~2^7-1
      * -128~127
      */
-    TYPE_BYTE("byte"),
+    BYTE("byte"),
 
     /**
      * 0~256
      */
-    TYPE_UBYTE("ubyte"),
-
-    /**
-     * 多位同选
-     */
-    TYPE_MULTI_BIT("multi_bit"),
-    /**
-     * 位
-     */
-    TYPE_BIT("bit");
+    UBYTE("ubyte");
 
     TypeEnum(String val) {
     }
-
 
     /**
      * 字符串匹配枚举类型
@@ -98,11 +57,6 @@ public enum TypeEnum {
      * @return 对应枚举
      */
     public static TypeEnum match(String value) {
-        String str = "TYPE_";
-        if (value.indexOf("|") > 0) {
-            value = value.replace("|", "_");
-        }
-        str += value.toUpperCase();
-        return valueOf(str);
+        return valueOf(value.toUpperCase());
     }
 }
