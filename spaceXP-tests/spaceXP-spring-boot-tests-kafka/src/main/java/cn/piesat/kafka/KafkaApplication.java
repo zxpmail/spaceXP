f0e1eb5893cdca8c0ad82501424faed69266444a
周小平 <zxpmail@163.com>
2024-10-18 14:27:04 +0800

    1、创建kafka测试程序

---
+++ spaceXP-tests/spaceXP-spring-boot-tests-kafka/src/main/java/cn/piesat/kafka/KafkaApplication.java
@@ -0,0 +1,19 @@
+package cn.piesat.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p/>
+ * {@code @description}  :启动程序
+ * <p/>
+ * <b>@create:</b> 2022/11/9 17:26.
+ *
+ * @author zhouxp
+ */
+@SpringBootApplication
+public class KafkaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaApplication.class,args);
+    }
+}
