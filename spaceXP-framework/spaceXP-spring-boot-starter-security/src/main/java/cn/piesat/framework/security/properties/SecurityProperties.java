dab87da44f5171b75d9eba33e172afc2ebc10649
周小平 <zxpmail@163.com>
2024-10-24 16:18:56 +0800

    1、增加license类

--- spaceXP-framework/spaceXP-spring-boot-starter-security/src/main/java/cn/piesat/framework/security/properties/SecurityProperties.java
+++ spaceXP-framework/spaceXP-spring-boot-starter-security/src/main/java/cn/piesat/framework/security/properties/SecurityProperties.java
@@ -1,5 +1,6 @@
 package cn.piesat.framework.security.properties;
 
+import cn.piesat.framework.security.model.LicenseVerifyParam;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
@@ -16,4 +17,23 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class SecurityProperties {
     private String secretKey = "123456";
     private Boolean enable = false;
+
+    private License license;
+
+    @Data
+    public static class License {
+        /**
+         * 证书subject
+         */
+        private String subject = "license";
+        /**
+         * 公钥别称
+         */
+        private String publicAlias = "publicCert";
+        /**
+         * 访问公钥库的密码
+         */
+        private String storePass = "public_password1234";
+    }
+
 }
