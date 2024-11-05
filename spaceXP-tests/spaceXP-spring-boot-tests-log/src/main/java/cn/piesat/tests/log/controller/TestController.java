f1f0dcd472287d7332f4112306d485f41d7bb4a0
周小平 <zxpmail@163.com>
2024-11-05 09:03:46 +0800

    1、取消Autowired配置

--- spaceXP-tests/spaceXP-spring-boot-tests-log/src/main/java/cn/piesat/tests/log/controller/TestController.java
+++ spaceXP-tests/spaceXP-spring-boot-tests-log/src/main/java/cn/piesat/tests/log/controller/TestController.java
@@ -10,7 +10,6 @@ import cn.piesat.tests.log.service.TestService;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
