88fbefeb286457459bc52aa4384b39941351e307
周小平 <zxpmail@163.com>
2024-12-06 14:09:18 +0800

    1、修改getMessage功能 2、增加获取底层异常信息 3、增加获取异常根功能 4、增加对代理报错功能

--- spaceXP-framework/spaceXP-common/src/main/java/cn/piesat/framework/common/utils/ExceptionUtil.java
+++ spaceXP-framework/spaceXP-common/src/main/java/cn/piesat/framework/common/utils/ExceptionUtil.java
@@ -2,9 +2,9 @@ package cn.piesat.framework.common.utils;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * <p/>
@@ -16,21 +16,52 @@ import java.io.StringWriter;
  */
 @Slf4j
 public class ExceptionUtil {
+    private ExceptionUtil() {
+    }
 
     /**
-     * 打印异常信息
+     * 将ErrorStack转化为String.
      */
-    public static String getMessage(Exception e) {
-        String swStr = null;
-        try (StringWriter sw = new StringWriter(); PrintWriter pw = new PrintWriter(sw)) {
-            e.printStackTrace(pw);
-            pw.flush();
-            sw.flush();
-            swStr = sw.toString();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            log.error(ex.getMessage());
+    public static String getMessage(Throwable ex) {
+        try (StringWriter stringWriter = new StringWriter();
+             PrintWriter printWriter = new PrintWriter(stringWriter)) {
+            ex.printStackTrace(printWriter);
+            return stringWriter.toString();
+        } catch (Exception e) {
+            throw new RuntimeException("Error while getting stack trace as string", e);
         }
-        return swStr;
+    }
+
+    /**
+     * 获取组合本异常信息与底层异常信息的异常描述, 适用于本异常为统一包装异常类，底层异常才是根本原因的情况。
+     */
+    public static String getErrorMessageWithNestedException(Throwable ex) {
+        Throwable nestedException = ex.getCause();
+        return ex.getMessage() + " nested exception is " +
+                nestedException.getClass().getName() + ":" + nestedException.getMessage();
+    }
+
+    /**
+     * 获取异常的Root Cause.
+     */
+    public static Throwable getRootCause(Throwable ex) {
+        Throwable cause;
+        Throwable result = null;
+        while ((cause = ex.getCause()) != null) {
+            result = cause;
+        }
+        return result;
+    }
+
+
+    /**
+     * 获取确切的异常信息：主要针对代理报错
+     */
+    public static Throwable getActualThrowable(final Throwable throwable) {
+        if (InvocationTargetException.class.equals(throwable.getClass())) {
+            InvocationTargetException exception = (InvocationTargetException) throwable;
+            return exception.getTargetException();
+        }
+        return throwable;
     }
 }
