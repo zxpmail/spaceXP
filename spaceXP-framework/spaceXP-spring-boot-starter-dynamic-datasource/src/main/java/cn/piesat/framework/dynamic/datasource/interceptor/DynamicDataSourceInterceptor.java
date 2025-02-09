9d877176e1eb3892f394ac2be6f0b1bce3172e98
周小平 <zxpmail@163.com>
2025-02-09 19:15:19 +0800

    1、实现查找数据源功能

--- spaceXP-framework/spaceXP-spring-boot-starter-dynamic-datasource/src/main/java/cn/piesat/framework/dynamic/datasource/interceptor/DynamicDataSourceInterceptor.java
+++ spaceXP-framework/spaceXP-spring-boot-starter-dynamic-datasource/src/main/java/cn/piesat/framework/dynamic/datasource/interceptor/DynamicDataSourceInterceptor.java
@@ -1,8 +1,13 @@
 package cn.piesat.framework.dynamic.datasource.interceptor;
 
+import cn.piesat.framework.dynamic.datasource.support.DataSourceClassResolver;
 import cn.piesat.framework.dynamic.datasource.utils.DynamicDataSourceContextHolder;
+import lombok.extern.slf4j.Slf4j;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
 
 /**
  * <p/>
@@ -11,7 +16,14 @@ import org.aopalliance.intercept.MethodInvocation;
  * {@code @create}: 2025-02-09 17:48
  * {@code @author}: zhouxp
  */
+@Slf4j
 public class DynamicDataSourceInterceptor implements MethodInterceptor {
+    private final List<DataSourceClassResolver> dataSourceClassResolvers;
+
+    public DynamicDataSourceInterceptor(List<DataSourceClassResolver> dataSourceClassResolvers) {
+        this.dataSourceClassResolvers = dataSourceClassResolvers;
+    }
+
     @Override
     public Object invoke(MethodInvocation invocation) throws Throwable {
         String dsKey = determineDataSourceKey(invocation);
@@ -24,6 +36,18 @@ public class DynamicDataSourceInterceptor implements MethodInterceptor {
     }
 
     private String determineDataSourceKey(MethodInvocation invocation) {
+        for (DataSourceClassResolver resolver : this.dataSourceClassResolvers) {
+            try {
+                String key = resolver.findKey(invocation.getMethod(), invocation.getThis());
+                if (StringUtils.hasText(key)) {
+                    return key;
+                }
+            } catch (Exception e) {
+                log.error("Error while finding data source key with resolver: " + resolver.getClass().getName(), e);
+            }
+        }
+
+        log.warn("No data source key found for method: " + invocation.getMethod().getName());
         return "";
     }
 }
---
+++ spaceXP-framework/spaceXP-spring-boot-starter-dynamic-datasource/src/main/java/cn/piesat/framework/dynamic/datasource/interceptor/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * <p/>
+ * {@code @description}:
+ * <p/>
+ * {@code @create}: 2025-02-09 19:09
+ * {@code @author}: zhouxp
+ */
+@NonNullApi
+package cn.piesat.framework.dynamic.datasource.interceptor;
+
+import org.springframework.lang.NonNullApi;
+
