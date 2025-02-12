73ef21f03282a6497931ece192fc88faf589a2aa
周小平 <zxpmail@163.com>
2025-02-12 16:35:26 +0800

    1、创建上下文工具类 2、实现事务工具类中的开始事务、提交和回滚功能

---
+++ spaceXP-framework/spaceXP-spring-boot-starter-dynamic-datasource/src/main/java/cn/piesat/framework/dynamic/datasource/transaction/TransactionConnectionContextHolder.java
@@ -0,0 +1,18 @@
+package cn.piesat.framework.dynamic.datasource.transaction;
+
+/**
+ * <p/>
+ * {@code @description}:事务Connection上下文工具类
+ * <p/>
+ * {@code @create}: 2025-02-12 16:31
+ * {@code @author}: zhouxp
+ */
+public class TransactionConnectionContextHolder {
+    public static void rollback() {
+
+    }
+
+    public static void commit() {
+
+    }
+}
--- spaceXP-framework/spaceXP-spring-boot-starter-dynamic-datasource/src/main/java/cn/piesat/framework/dynamic/datasource/transaction/TransactionUtil.java
+++ spaceXP-framework/spaceXP-spring-boot-starter-dynamic-datasource/src/main/java/cn/piesat/framework/dynamic/datasource/transaction/TransactionUtil.java
@@ -1,5 +1,9 @@
 package cn.piesat.framework.dynamic.datasource.transaction;
 
+import org.springframework.util.StringUtils;
+
+import java.util.UUID;
+
 /**
  * <p/>
  * {@code @description}: 事务工具类
@@ -9,12 +13,18 @@ package cn.piesat.framework.dynamic.datasource.transaction;
  */
 public class TransactionUtil {
     public static void startTransaction() {
-
+        String txGlobalId = TransactionGlobalIdContextHolder.getTxGlobalId();
+        if (!StringUtils.hasText(txGlobalId)) {
+            txGlobalId = UUID.randomUUID().toString();
+            TransactionGlobalIdContextHolder.setTxGlobalId(txGlobalId);
+        }
     }
 
     public static void rollback() {
+        TransactionConnectionContextHolder.rollback();
     }
 
     public static void commit() {
+        TransactionConnectionContextHolder.commit();
     }
 }
