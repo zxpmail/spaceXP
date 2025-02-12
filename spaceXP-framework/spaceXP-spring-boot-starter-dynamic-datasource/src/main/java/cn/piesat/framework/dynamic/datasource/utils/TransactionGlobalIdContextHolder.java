fe43f44cf219b3937af16d0198a0b94d1dbfcc43
周小平 <zxpmail@163.com>
2025-02-12 15:25:05 +0800

    1、增加全局事务ID上下文工具类

---
+++ spaceXP-framework/spaceXP-spring-boot-starter-dynamic-datasource/src/main/java/cn/piesat/framework/dynamic/datasource/utils/TransactionGlobalIdContextHolder.java
@@ -0,0 +1,27 @@
+package cn.piesat.framework.dynamic.datasource.utils;
+
+/**
+ * <p/>
+ * {@code @description}:全局事务ID上下文工具类
+ * <p/>
+ * {@code @create}: 2025-02-12 15:22
+ * {@code @author}: zhouxp
+ */
+public class TransactionGlobalIdContextHolder {
+    private static final ThreadLocal<String> TX_GLOBAL_ID = new ThreadLocal<>();
+
+    /**
+     * 获取全局事务id
+     */
+    public static String getTxGlobalId() {
+        return TX_GLOBAL_ID.get();
+    }
+
+    public static void setTxGlobalId(String txId) {
+        TX_GLOBAL_ID.set(txId);
+    }
+
+    public static void removeGlobalId() {
+        TX_GLOBAL_ID.remove();
+    }
+}
