f9a7957f1fc55b898e20f94e001d386026486b9f
周小平 <zxpmail@163.com>
2025-02-18 16:14:37 +0800

    1、增加包装请求类

---
+++ spaceXP-framework/spaceXP-spring-boot-starter-mybatis-plus-external/src/main/java/cn/piesat/framework/mybatis/plus/external/core/WrapRequest.java
@@ -0,0 +1,28 @@
+package cn.piesat.framework.mybatis.plus.external.core;
+
+import lombok.Data;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * <p/>
+ * {@code @description}: 包装请求类
+ * <p/>
+ * {@code @create}: 2025-02-18 16:09
+ * {@code @author}: zhouxp
+ */
+@Data
+public final class WrapRequest<T,R> {
+    /**
+     * 标识ID
+     */
+    String id;
+    /**
+     * 数据域ID
+     */
+    T dataId;
+    /**
+     * 数据结果域
+     */
+    LinkedBlockingQueue<R> resultQueue;
+}
