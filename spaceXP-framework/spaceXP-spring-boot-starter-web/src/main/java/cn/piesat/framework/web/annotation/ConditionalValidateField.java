a6d9f37c239d253ce256896b9afbf6d99adee51e
zhouxp <zxpmail@163.com>
2025-02-26 16:24:55 +0800

    修改字段条件注解

--- spaceXP-framework/spaceXP-spring-boot-starter-web/src/main/java/cn/piesat/framework/web/annotation/ConditionalValidateField.java
+++ spaceXP-framework/spaceXP-spring-boot-starter-web/src/main/java/cn/piesat/framework/web/annotation/ConditionalValidateField.java
@@ -1,5 +1,7 @@
 package cn.piesat.framework.web.annotation;
 
+import java.lang.annotation.*;
+
 /**
  * <p/>
  * {@code @description}: 验证字段条件注解
@@ -7,5 +9,28 @@ package cn.piesat.framework.web.annotation;
  * {@code @create}: 2025-02-26 16:16:34
  * {@code @author}: zhouxp
  */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface ConditionalValidateField {
+
+    /**
+     * 关联字段
+     **/
+    String relationField();
+
+    /**
+     * 要执行的的校验动作
+     **/
+    int action();
+
+    /**
+     * 值
+     * **/
+    String ifValue() default "";
+
+    /**
+     * 信息
+     **/
+    String message() default "";
 }
