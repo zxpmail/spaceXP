6fae8b2b613f0900c2c0e0c64d63967b186146f6
zhouxp <zxpmail@163.com>
2025-02-26 09:18:35 +0800

    重写合并功能

--- spaceXP-framework/spaceXP-spring-boot-starter-mybatis-plus-external/src/main/java/cn/piesat/framework/mybatis/plus/external/core/BatchQueryQueueService.java
+++ spaceXP-framework/spaceXP-spring-boot-starter-mybatis-plus-external/src/main/java/cn/piesat/framework/mybatis/plus/external/core/BatchQueryQueueService.java
@@ -1,5 +1,6 @@
 package cn.piesat.framework.mybatis.plus.external.core;
 
+
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.*;
@@ -16,56 +17,58 @@ import java.util.function.Function;
  * {@code @create}: 2025-02-24 19:44:35
  * {@code @author}: zhouxp
  */
-@SuppressWarnings({"rawtypes","unchecked"})
 @Slf4j
-public abstract class BatchQueryQueueService <T,R> {
-    int MAX_TASK_NUM = 100;
-    Queue<WrapRequest > queue = new LinkedBlockingQueue<>();
-    protected void init(Function<List<WrapRequest>, Map<String, R>> fun) {
-        //定时任务线程池,创建一个支持定时、周期性或延时任务的限定线程数目(这里传入的是1)的线程池
-        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
-        scheduledExecutorService.scheduleAtFixedRate(()->{
-            int size = queue.size();
-            //如果队列没数据,表示这段时间没有请求,直接返回
-            if (size == 0) {
-                return;
-            }
-            List<WrapRequest> list = new ArrayList<>();
-            log.info("合并了 {} 个请求",size);
-            //将队列的请求消费到一个集合保存
-            for (int i = 0; i < size; i++) {
-                // 后面的SQL语句是有长度限制的，所以还要做限制每次批量的数量,超过最大任务数，等下次执行
-                if (i < MAX_TASK_NUM) {
-                    list.add(queue.poll());
+public  abstract class  BatchQueryQueueService <T,R> {
+
+    private static final int MAX_TASK_NUM = 100;
+    private final ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+    private final LinkedBlockingQueue<WrapRequest<T, R>> QUEUE = new LinkedBlockingQueue<>();
+    protected void init(Function<List<WrapRequest<T, R>>, Map<String, R>> fun) {
+        scheduledExecutorService.scheduleAtFixedRate(() -> {
+            try {
+                List<WrapRequest<T, R>> list = new ArrayList<>();
+                // 使用 drainTo 方法批量取出元素，避免部分任务被丢弃
+                QUEUE.drainTo(list, MAX_TASK_NUM);
+
+                if (list.isEmpty()) {
+                    return;
                 }
-            }
-            //拿到我们需要去数据库查询的特征,保存为集合
-            List<WrapRequest> reqs = new ArrayList<>(list);
-            Map<String, R> response =fun(reqs);
-            for (WrapRequest req : reqs) {
-                // 这里再把结果放到队列里
-                R r = response.get(req.getId());
-                req.resultQueue.offer(r);
+
+                log.info("合并了 {} 个请求", list.size());
+
+                // 拿到我们需要去数据库查询的特征,保存为集合
+                Map<String, R> response = fun.apply(list);
+                for (WrapRequest<T, R> req : list) {
+                    // 这里再把结果放到队列里
+                    R r = response.get(req.getId());
+                    if (r != null) {
+                        req.resultQueue.offer(r);
+                    } else {
+                        log.info("未找到请求 {} 的结果", req.getId());
+                    }
+                }
+            } catch (Exception e) {
+                log.error("定时任务执行失败: " + e.getMessage(), e);
             }
         }, 100, 10, TimeUnit.MILLISECONDS);
     }
 
-    protected abstract Map<String,R> fun(List<WrapRequest> reqs);
 
-    protected R queryR(T id) {
-        WrapRequest request = new WrapRequest();
+    protected R queryResult(T id) {
+        WrapRequest<T, R> request = new WrapRequest<>();
         // 这里用UUID做请求id
         request.id = UUID.randomUUID().toString().replace("-", "");
         request.dataId = id;
         LinkedBlockingQueue<R> resultQueue = new LinkedBlockingQueue<>();
         request.resultQueue = resultQueue;
-        //将对象传入队列
-        queue.offer(request);
-        //取出元素时，如果队列为空，给定阻塞多少毫秒再队列取值，这里是3秒
+        // 将对象传入队列
+        QUEUE.offer(request);
         try {
-            return resultQueue.poll(3000,TimeUnit.MILLISECONDS);
+            // 取出元素时，如果队列为空，给定阻塞多少毫秒再队列取值，这里是3秒
+            return resultQueue.poll(3000, TimeUnit.MILLISECONDS);
         } catch (InterruptedException e) {
-            log.error("查询超时",e);
+            Thread.currentThread().interrupt(); // 恢复中断状态
+            log.error("查询超时 " ,e);
         }
         return null;
     }
