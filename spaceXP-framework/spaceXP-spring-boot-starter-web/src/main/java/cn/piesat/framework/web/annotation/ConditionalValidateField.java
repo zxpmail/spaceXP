6e59909d98ad97e5605beb0291f8d8dd9ecc96c9
zhouxp <zxpmail@163.com>
2025-02-26 16:19:32 +0800

    验证字段条件注解

---
+++ spaceXP-framework/spaceXP-spring-boot-starter-web/src/main/java/cn/piesat/framework/web/annotation/ConditionalValidateField.java
@@ -0,0 +1,11 @@
+package cn.piesat.framework.web.annotation;
+
+/**
+ * <p/>
+ * {@code @description}: 验证字段条件注解
+ * <p/>
+ * {@code @create}: 2025-02-26 16:16:34
+ * {@code @author}: zhouxp
+ */
+public @interface ConditionalValidateField {
+}
