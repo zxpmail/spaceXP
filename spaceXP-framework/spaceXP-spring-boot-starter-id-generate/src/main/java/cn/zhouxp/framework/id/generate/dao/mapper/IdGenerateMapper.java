f017cd386744260091b923d3e32d8d6a0515e581
zhouxp <zxpmail@163.com>
2025-03-05 20:23:41 +0800

    1、增加id生成器Mapper

---
+++ spaceXP-framework/spaceXP-spring-boot-starter-id-generate/src/main/java/cn/zhouxp/framework/id/generate/dao/mapper/IdGenerateMapper.java
@@ -0,0 +1,17 @@
+package cn.zhouxp.framework.id.generate.dao.mapper;
+
+import cn.zhouxp.framework.id.generate.dao.po.IdGeneratePO;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p/>
+ * {@code @description}  :
+ * <p/>
+ * <b>@create:</b> 2025-03-05 20:21:50
+ *
+ * @author zhouxp
+ */
+@Mapper
+public interface IdGenerateMapper extends BaseMapper<IdGeneratePO> {
+}
