7b03a58efe345379a7b4beac5c0a97705dc3c3b5
zhouxp <zxpmail@163.com>
2025-03-05 20:30:08 +0800

    1、增加id生成器服务文件

---
+++ spaceXP-framework/spaceXP-spring-boot-starter-id-generate/src/main/java/cn/zhouxp/framework/id/generate/service/IdGenerateService.java
@@ -0,0 +1,12 @@
+package cn.zhouxp.framework.id.generate.service;
+
+/**
+ * <p/>
+ * {@code @description}  :
+ * <p/>
+ * <b>@create:</b> 2025-03-05 20:26:51
+ *
+ * @author zhouxp
+ */
+public interface IdGenerateService {
+}
---
+++ spaceXP-framework/spaceXP-spring-boot-starter-id-generate/src/main/java/cn/zhouxp/framework/id/generate/service/impl/IdGenerateServiceImpl.java
@@ -0,0 +1,22 @@
+package cn.zhouxp.framework.id.generate.service.impl;
+
+import cn.zhouxp.framework.id.generate.dao.mapper.IdGenerateMapper;
+import cn.zhouxp.framework.id.generate.service.IdGenerateService;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p/>
+ * {@code @description}  :
+ * <p/>
+ * <b>@create:</b> 2025-03-05 20:28:03
+ *
+ * @author zhouxp
+ */
+@Service
+public class IdGenerateServiceImpl implements IdGenerateService {
+    @Setter(onMethod_ = @Autowired)
+    private IdGenerateMapper idGenerateMapper;
+
+}
